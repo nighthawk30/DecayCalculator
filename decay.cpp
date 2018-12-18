e3d27cc9f5d4a0fbbd2feeb4950fe4123bde899d
U-BSD\385975 <385975@SHS-10DD3C429.beaverton.k12.or.us>
2018-12-18 11:48:23 -0800

    It has begun

--- decay.cpp
+++ decay.cpp
@@ -5,9 +5,24 @@ Nuclear Decay Calculator - using Taylor Polynomials
 
 #include <iostream>
 #include <cstdlib>
-#include <cmeth>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+void printCommands();
 
 int main()
 {
+  
   return 0;
+}
+
+void printCommands()
+{
+  cout << "'add' - Add a node to the tree" << endl;
+  cout << "'delete' - Remove a node from the tree" << endl;
+  cout << "'print' - Print out the tree structure" << endl;
+  cout << "'help' - Print out the list of commands" << endl;
+  cout << "'quit' - Exit the program" << endl;
 }
