2e5755428a4d3e9b5d4b96ba27515aaa0ccb9648
unknown <nt.rip.diver@gmail.com>
2018-12-26 14:15:45 +0000

    Center, Derivative & Integral

--- decay.cpp
+++ decay.cpp
@@ -14,20 +14,146 @@ h is the half life of the element and N is the intial amount
 using namespace std;
 
 void printCommands();
+void centerMaclaurin(int iterations, float time, float half, float initial);
+void derivativeMaclaurin(int iterations, float time, float half, float initial);
+void integralMaclaurin(int iterations, float time, float half, float initial);
+double factorial(double number);
 
 int main()
 {
+  char* input = new char[80];
+  bool end = false;
+  cout << "This program will use Taylor Polynomials to calculate the amount of an element that remains after it experiences nuclear decay" << endl;
+  printCommands();
+  cout << endl;
+  while (!end)
+    {
+      float initial;
+      cout << "Initial Quantity: ";
+      cin.getline(input, 80);
+      initial = atof(input);
+
+      float half;
+      cout << "Half Life: ";
+      cin.getline(input, 80);
+      half = atof(input);
   
+      while(true)
+	{
+	  cout << endl << "Command: ";
+	  cin.getline(input, 80);
+	  if (strcmp(input, "new") == 0)
+	    {
+	      break;
+	    }
+	  else if (strcmp(input, "quit") == 0)
+	    {
+	      end = true;
+	      break;
+	    }
+	  else if (strcmp(input, "help") == 0)
+	    {
+	      printCommands();
+	    }
+	  else if (strcmp(input, "center") == 0)
+	    {
+	      cout << "Time: ";
+	      cin.getline(input, 80);
+	      int time = atoi(input);
+
+	      cout << "Iterations: ";
+	      cin.getline(input, 80);
+	      centerMaclaurin(atof(input), time, half, initial);
+	    }
+	  else if (strcmp(input, "derivative") == 0)
+	    {
+	      cout << "Time: ";
+	      cin.getline(input, 80);
+	      int time = atoi(input);
+
+	      cout << "Iterations: ";
+	      cin.getline(input, 80);
+	      derivativeMaclaurin(atof(input), time, half, initial);
+	    }
+	  else if (strcmp(input, "integral") == 0)
+	    {
+	      cout << "Time: ";
+	      cin.getline(input, 80);
+	      int time = atoi(input);
+
+	      cout << "Iterations: ";
+	      cin.getline(input, 80);
+	      integralMaclaurin(atof(input), time, half, initial);
+	    }
+	}
+    }
   return 0;
 }
 
+// e^x = 1 + x/1! + x^2/2! + x^3/3!...
+void centerMaclaurin(int iterations, float time, float half, float initial)
+{
+  float rate = .693*time/half;
+  float remaining = 0;
+  for (int i = 0; i < iterations; i++)
+    {
+      remaining += pow(rate, i)/factorial(double(i));
+      //cout << pow(rate, i) << " " << factorial(double(i)) << " " << pow(rate, i)/factorial(double(i)) << endl;
+    }
+  initial /= remaining;
+  cout << initial << endl;
+  return;
+}
 
+//get the slope 0+ 2x^1/2! + 3x^2/3! + (x^3)/3!+5x^4/(5x4!)
+void derivativeMaclaurin(int iterations, float time, float half, float initial)
+{
+  float rate = .693*time/half;
+  //cout << rate << endl;
+  float remaining = 0;
+  for (int i = 1; i < iterations; i++)
+    {
+      remaining += i*pow(rate, i-1)/factorial(double(i));
+      //cout << pow(rate, i) << " " << factorial(double(i)) << " " << pow(rate, i)/factorial(double(i)) << endl;
+    }
+  initial /= remaining;
+  cout << initial << endl;
+  return;  
+}
+
+//get the slope x^1/1*0! + x^2/(2*1!) + x^3/(3*2!)...from x^n/n! to x^(n+1)/((n+1)n!)
+void integralMaclaurin(int iterations, float time, float half, float initial)
+{
+  float rate = .693*time/half;
+  //cout << rate << endl;
+  float remaining = 0;
+  for (int i = 0; i < iterations; i++)
+    {
+      remaining += pow(rate, i+1)/((i+1)*factorial(double(i)));
+      //cout << pow(rate, i) << " " << factorial(double(i)) << " " << pow(rate, i)/factorial(double(i)) << endl;
+    }
+  initial /= remaining;
+  cout << initial << endl;
+  return;  
+}
+
+
+double factorial(double number)
+{
+  double fact = 1;
+  while (number > 0)
+    {
+      fact *= number--;
+    }
+  return fact;
+}
 
 void printCommands()
 {
-  cout << "'add' - Add a node to the tree" << endl;
-  cout << "'delete' - Remove a node from the tree" << endl;
-  cout << "'print' - Print out the tree structure" << endl;
+  cout << "'center' - Calculate the amount left at a time" << endl;
+  cout << "'integral' - Calculate the integral at a time" << endl;
+  cout << "'derivative' - Calculate the decay rate at a time" << endl;
+  cout << "'new' - Start a new decay equation" << endl;
   cout << "'help' - Print out the list of commands" << endl;
-  cout << "'quit' - Exit the program" << endl;
+  cout << "'quit' - Exit from the program" << endl;
 }
